d91b1932738f461edb1c54272d6dc741acf134f6
Guilherme Miranda <guilherme.miranda@gruponazca.com.br>
2020-07-20 11:38:40 -0300

    Segundo teste de import

--- assets/js/imports.js
+++ assets/js/imports.js
@@ -8,3 +8,14 @@ fetch("./navbar.html")
 .catch(function(error){
   console.log(error);
 })
+
+fetch("./footer.html")
+.then(response => {
+  return response.text()
+})
+.then(data => {;
+  document.querySelector("footer").innerHTML = data;
+})
+.catch(function(error){
+  console.log(error);
+})
