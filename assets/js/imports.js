687265b6c98fa95694f84e5d844ed7f4a40d4d5d
Guilherme Miranda <guilherme.miranda@gruponazca.com.br>
2020-07-20 11:21:24 -0300

    Segundo teste de import

---
+++ assets/js/imports.js
@@ -0,0 +1,10 @@
+fetch("../../navbar.html")
+.then(response => {
+  return response.text()
+})
+.then(data => {;
+  document.querySelector("navbar").innerHTML = data;
+})
+.catch(function(error){
+  console.log(error);
+})
