f07d5dcdbab815814311640fae0133d5c4f73814
Guilherme Miranda <guilherme.miranda@gruponazca.com.br>
2020-08-06 14:48:16 -0300

    Pequenas correcoes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
     gutter:10,
     controlsText: ['<ion-icon name="chevron-back" class="botao-slider" size="large"></ion-icon>', '<ion-icon name="chevron-forward" class="botao-slider" size="large"></ion-icon>'],
     nav:false,
+    slideBy:'page',
     responsive: {
       900: {
         items: 3
@@ -18,9 +19,11 @@ $(document).ready(function(){
     autoHeight:true,
     container: '.precos-slider',
     items: 1,
-    gutter:10,
-    controlsText: ['<ion-icon name="chevron-back" class="botao-slider" size="large"></ion-icon>', '<ion-icon name="chevron-forward" class="botao-slider" size="large"></ion-icon>'],
+    gutter:20,
+    controlsText: ['<ion-icon name="chevron-back" class="botao-slider-preco" size="large"></ion-icon>', '<ion-icon name="chevron-forward" class="botao-slider-preco" size="large"></ion-icon>'],
     nav:false,
+    loop:false,
+    slideBy:'page',
     responsive: {
       900: {
         items: 3
